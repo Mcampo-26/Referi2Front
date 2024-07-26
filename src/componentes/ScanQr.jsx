cdc2d931a96d61cbaaeb1c6b9c4b3ab5987747a1
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-07-26 11:01:48 -0300

    Update ScanQr.jsx

--- src/componentes/ScanQr.jsx
+++ src/componentes/ScanQr.jsx
@@ -95,8 +95,8 @@ export const ScanQr = () => {
     scannerRef.current.start(
       { facingMode: "environment" },
       {
-        fps: 80, // Aumenta FPS para mejorar la calidad del escaneo
-        qrbox: { width: 280, height: 280 }, // Aumenta el tamaño del área de escaneo
+        fps: 8, // Aumenta FPS para mejorar la calidad del escaneo
+        qrbox: { width: 300, height: 300 }, // Aumenta el tamaño del área de escaneo
         disableFlip: true // Desactiva el parpadeo al girar la cámara
       },
       handleScan,
