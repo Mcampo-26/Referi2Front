5357e56b821c4ec8dd20e57163e9bcf0961335e4
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-08-09 12:09:33 -0300

    Update config.js

--- src/utilities/config.js
+++ src/utilities/config.js
@@ -3,9 +3,9 @@
 
 
 
-//export const URL = 'https://referido2back.onrender.com'
+export const URL = 'https://referido2back.onrender.com'
 
 
 
-export const URL = "http://localhost:8080"
+//export const URL = "http://localhost:8080"
 
