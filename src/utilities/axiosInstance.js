420fc5c624139dc32dd4eca0ffeed519fa3a4a63
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-08-20 17:02:00 -0300

    Update axiosInstance.js

--- src/utilities/axiosInstance.js
+++ src/utilities/axiosInstance.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-import { URL, NGROK_URL } from './config'; // Importa las URLs desde config.js
+import { URL } from './config'; // Importa las URLs desde config.js
 
 const axiosInstance = axios.create({
   baseURL: URL,  // URL base general predeterminada
@@ -12,11 +12,6 @@ axiosInstance.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  // Cambiar la baseURL a NGROK_URL solo si la URL incluye '/create_preference'
-  if (config.url.includes('/create_preference')) {
-    config.baseURL = NGROK_URL;
   }
 
   return config;
