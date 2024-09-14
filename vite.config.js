2744986c09bee7ed821e2effae5bad7988e54085
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-09-14 10:35:48 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,13 @@ import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    target: 'esnext', // O puedes usar 'es2022'
+  },
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080', // Cambia esto al puerto de tu backend
+        target: 'http://https://main--referi2.netlify.app', // Cambia esto al puerto de tu backend
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
