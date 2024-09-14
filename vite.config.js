e2e86cd9bc124294de5f385cd1d76b781d6834af
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-09-14 10:46:08 -0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,19 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import { URL } from './src/utilities/config'; // Asegúrate de que el archivo de configuración esté correctamente configurado.
+import { URL } from './src/utilities/config';
 
 export default defineConfig({
   plugins: [react()],
   esbuild: {
-    target: 'esnext', // O puedes usar 'es2022'
+    target: 'esnext', // o 'es2022'
+    supported: {
+      'top-level-await': true, // Habilitar top-level await
+    },
   },
   server: {
     proxy: {
       '/api': {
-        target: URL, // URL del backend
+        target: URL, // Asegúrate de que esta sea la URL correcta
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
