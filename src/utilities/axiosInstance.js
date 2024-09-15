b58088210e03fe0b7b22e7ff3702269559e9042a
Mcampo-26 <77281924+Mcampo-26@users.noreply.github.com>
2024-09-15 13:40:39 -0300

    Update axiosInstance.js

--- src/utilities/axiosInstance.js
+++ src/utilities/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const jwt_decode = (await import('jwt-decode')).default;
-import { URL } from './config'; // Importa la URL
+import { URL } from './config'; // Importa ambas URLs
 
 const axiosInstance = axios.create({
   baseURL: URL,
@@ -26,17 +26,10 @@ axiosInstance.interceptors.request.use(
   async (config) => {
     // Cambia la base URL a NGROK_URL si la ruta es /createPayment
     if (config.url.includes('/createPayment')) {
-      config.baseURL = URL;
+      config.baseURL =URL;
     }
 
     let token = localStorage.getItem('token');
-    if (token && isTokenExpired(token)) {
-      console.warn('Token expirado. Redirigiendo al inicio de sesión...');
-      localStorage.removeItem('token'); // Eliminar el token expirado
-      window.location.href = '/login'; // Redirigir al usuario al inicio de sesión
-      return Promise.reject('Token expirado');
-    }
-
     if (!token) {
       await new Promise(resolve => setTimeout(resolve, 50)); // Retraso de 50ms
       token = localStorage.getItem('token');
